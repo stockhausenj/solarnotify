101222db264dbab55861c5c89e1d2b12ff17e19e
Jay Stockhausen <jay.stockhausen@gmail.com>
2024-10-29 15:28:36 -0600

    init

---
+++ solarnotify/src/App.tsx
@@ -0,0 +1,18 @@
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://solarnotify.com" target="_blank">
+          <img src={logo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>SolarNotify</h1>
+    </>
+  )
+}
+
+export default App
