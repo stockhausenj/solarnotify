e431afb14c1e130665d8596b0abb374bed5ac34d
Jay Stockhausen <jay.stockhausen@gmail.com>
2024-11-11 14:45:30 -0700

    add title to graph

--- solarnotify/src/components/LiveData/LiveData.tsx
+++ solarnotify/src/components/LiveData/LiveData.tsx
@@ -1,46 +1,50 @@
-import { Space } from '@mantine/core';
+import { Title, Space } from '@mantine/core';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export function LiveData() {
-	type ChartData = {
-		name: string;
-		count: number;
-	};
+  type ChartData = {
+    name: string;
+    count: number;
+  };
 
-	const [data, setData] = useState<ChartData[]>([]);
+  const [data, setData] = useState<ChartData[]>([]);
 
-	const fetchData = async () => {
-		try {
-			const response = await axios.get('/api/livedata/status');
-			setData(response.data);
-		} catch (error) {
-			console.error('Error fetching data:', error);
-		}
-	};
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('/api/livedata/status');
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-	useEffect(() => {
-		fetchData();
+  useEffect(() => {
+    fetchData();
 
-		const intervalId = setInterval(fetchData, 30000);
+    const intervalId = setInterval(fetchData, 30000);
 
-		return () => clearInterval(intervalId);
-	}, []);
+    return () => clearInterval(intervalId);
+  }, []);
 
-	return (
-		<>
-			<Space h="lg" />
-			<ResponsiveContainer width="100%" height={400}>
-				<BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="name" />
-					<YAxis />
-					<Tooltip />
-					<Legend />
-					<Bar dataKey="count" fill="#8884d8" />
-				</BarChart>
-			</ResponsiveContainer>
-		</>
-	);
+  return (
+    <>
+      <Space h="lg" />
+      <Title order={5}>
+        Status
+      </Title>
+      <Space h="lg" />
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="count" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
+  );
 }
